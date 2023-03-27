ef45756afc019c3ddab5af4d7001a667f699332e
Jose Carlos <ljosecarlos295@gmail.com>
2023-03-27 00:32:40 -0500

    Add conexion with BD sql-server, in addition, the product api was created

--- Tiendaapi/Controllers/ProductosController.cs
+++ Tiendaapi/Controllers/ProductosController.cs
@@ -1,6 +1,20 @@
-﻿namespace Tiendaapi.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Tiendaapi.Datos;
+using Tiendaapi.Modelo;
+
+namespace Tiendaapi.Controllers
 {
+    [ApiController]
+    [Route("api/productos")]
     public class ProductosController
     {
+        [HttpGet]
+        public async Task <ActionResult<List<Mproductos>>> Get()
+        {
+            var funcion = new Dproductos();
+            var lista = await funcion.MostrarProductos();
+            return lista;
+            
+        }
     }
 }
