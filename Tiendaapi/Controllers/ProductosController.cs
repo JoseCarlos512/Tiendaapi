c801acbdfe16d213c2378240967a1654b861eff3
Jose Carlos <ljosecarlos295@gmail.com>
2023-04-09 01:38:43 -0500

    Agregar restos de metodos para completar el CRUD (REST)

--- Tiendaapi/Controllers/ProductosController.cs
+++ Tiendaapi/Controllers/ProductosController.cs
@@ -6,7 +6,7 @@ namespace Tiendaapi.Controllers
 {
     [ApiController]
     [Route("api/productos")]
-    public class ProductosController
+    public class ProductosController: ControllerBase
     {
         [HttpGet]
         public async Task <ActionResult<List<Mproductos>>> Get()
@@ -15,6 +15,33 @@ namespace Tiendaapi.Controllers
             var lista = await funcion.MostrarProductos();
             return lista;
             
+        }
+
+        [HttpPost]
+        public async Task Post([FromBody] Mproductos parametros)
+        {
+            var funcion = new Dproductos();
+            await funcion.InsertarProductos(parametros);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] Mproductos parametros)
+        {
+            var funcion = new Dproductos();
+            parametros.id = id;
+
+            await funcion.UpdateProductos(parametros);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> delete(int id)
+        {
+            var funcion = new Dproductos();
+            var parametros = new Mproductos();
+            parametros.id = id;
+            await funcion.DeleteProductos(parametros);
+            return NoContent();
         }
     }
 }
--- Tiendaapi/Datos/Dproductos.cs
+++ Tiendaapi/Datos/Dproductos.cs
@@ -7,15 +7,22 @@ namespace Tiendaapi.Datos
 {
     public class Dproductos
     {
+        /** Conexion a la BD **/
         Conexionbd cn = new Conexionbd();
+
+        /** Mostrar productos **/
         public async Task <List<Mproductos>> MostrarProductos()
         {
             var lista = new List<Mproductos>();
             using (var sql = new SqlConnection(cn.cadenaSQL()))
             {
+                // Se conecta al procedure con SqlCommand
                 using (var cmd = new SqlCommand("mostrarProductos", sql))
                 {
+                    // Como ya tiene listo la instancia el nombre del procedure
+                    // Abre la conexion
                     await sql.OpenAsync();
+                    // Ejecuta el procedure
                     cmd.CommandType = CommandType.StoredProcedure;
 
                     using(var item = await cmd.ExecuteReaderAsync()) 
@@ -33,6 +40,57 @@ namespace Tiendaapi.Datos
                 }
             }
             return lista;
+        }
+
+        /** Insertar productos **/
+        public async Task InsertarProductos(Mproductos parametros)
+        {
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("insertarProductos", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("descripcion", parametros.descripcion);
+                    cmd.Parameters.AddWithValue("precio", parametros.precio);
+
+                    await sql.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+        }
+
+
+        /** Update productos **/
+        public async Task UpdateProductos(Mproductos parametros)
+        {
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("editarProductos", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("id", parametros.id);
+                    cmd.Parameters.AddWithValue("precio", parametros.precio);
+
+                    await sql.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+        }
+
+        /** Delete productos **/
+        public async Task DeleteProductos(Mproductos parametros)
+        {
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("eliminarProductos", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("id", parametros.id);
+
+                    await sql.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
         }
     }
 }
